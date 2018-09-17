fea532e19968fd6c6aef060338a34d1c2962cbb2
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-17 03:51:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,11 +14,11 @@ end
     return cart
     session["form"] = "newForm"
   		erb :index
-
-      if session[:lastname]
-    erb :"orders/drinks"
-      #params from the form and add it to the session hash. The key should be
-      #item and the value should be the item the user entered to the purchase. Make sure to
-      # store the session hash in an instance variable that you can access in the views.
   	end
+
+
+    GET '/hey' do 
+  session["purchase"] = "item"
+  @session = session
+end
 end
