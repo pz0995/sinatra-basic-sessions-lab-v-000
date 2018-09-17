510e10d04a3e22b2144048ab37d0c65ce95a3eeb
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-17 03:34:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,6 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-
-
   configure do
   enable :sessions
   set :session_secret, "secret"
@@ -12,6 +9,7 @@ end
   GET "/" do
   session["form"] = "newForm"
   #renders a form to buy somethibng
+  if session[:lastname]
 		erb :item
 	end
 
@@ -19,6 +17,9 @@ end
     return cart
     session["form"] = "newForm"
   		erb :index
+
+      if session[:lastname]
+    erb :"orders/drinks"
       #params from the form and add it to the session hash. The key should be
       #item and the value should be the item the user entered to the purchase. Make sure to
       # store the session hash in an instance variable that you can access in the views.
