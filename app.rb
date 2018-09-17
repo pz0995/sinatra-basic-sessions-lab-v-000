f612c35219dd52baca438dfa82eefb760c78b1a1
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-17 03:51:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+  GET "/" do
+  session["form"] = "newForm"
+	end
+
+  POST "/checkout" do
+    return cart
+    session["form"] = "newForm"
+  		erb :index
+  	end
+
+
+    GET '/hey' do
+  session["purchase"] = "item"
+  @session = session
+end
 end
